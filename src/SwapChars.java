2bc336c215c4433b3be37ebbe3f00da7c32b07aa
sdlaw88 <sdlaw88@gmail.com>
2020-01-05 14:06:08 -0500

    swap two chars test

--- src/SwapChars.java
+++ src/SwapChars.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class SwapChars {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner (System.in);
+		
+		
+	}
+
+	public static String swap2(String str) {
+		// TODO Auto-generated method stub
+		
+		char char1 = str.charAt(0);
+		char char2 = str.charAt(1);
+		
+		return "" + char2 +char1;
 	}
 
 }
---
+++ src/SwapCharsTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SwapCharsTest {
+
+	@Test
+	void swapStringWith2Chars() {
+		assertEquals("AB",SwapChars.swap2("BA"));
+	}
+		@Test
+		void swapStringWith6Chars() {
+			assertEquals("AB",SwapChars.swap2("BA"));
+	
+}
+}
